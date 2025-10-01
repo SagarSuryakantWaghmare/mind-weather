c27899fef461d039de3946fdc9d98dfc0200aec2
Sagar <sagarwaghmare1384@gmail.com>
2025-10-01 23:26:05 +0530

    Db connect and the model added of logs and analysis

---
+++ utils/db.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+
+
+// This 'as unknown as' just a typescript that we can convert if it don't known 
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClient | undefined
+}
+
+
+export const prisma =
+    globalForPrisma.prisma ??
+    new PrismaClient({
+        log: ['query'],
+    })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma=prisma;
